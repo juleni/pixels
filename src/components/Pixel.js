e9fc8e944e64f73d829a10ed652b4a57d41a283f
juleni <69912179+juleni@users.noreply.github.com>
2022-12-14 11:06:00 +0100

    Implemented components Pixel and Row

--- src/components/Pixel.js
+++ src/components/Pixel.js
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/pixel.scss";
 
-export default function Pixel() {
-  return <div className="pixel">Pixel</div>;
+export default function Pixel(props) {
+  const { selectedColor } = props;
+
+  const [pixelColor, setPixelColor] = useState("#fff");
+  const [oldColor, setOldColor] = useState(pixelColor);
+  const [canChangeColor, setCanChangeColor] = useState(true);
+
+  function applyColor() {
+    setPixelColor(selectedColor);
+    setCanChangeColor(false);
+  }
+  function changeColorOnHover() {
+    setOldColor(pixelColor);
+    setPixelColor(selectedColor);
+  }
+  function reset() {
+    if (canChangeColor) {
+      setPixelColor(oldColor);
+    }
+    setCanChangeColor(true);
+  }
+
+  return (
+    <div
+      className="pixel"
+      style={{ backgroundColor: pixelColor }}
+      onClick={applyColor}
+      onMouseEnter={changeColorOnHover}
+      onMouseLeave={reset}
+    ></div>
+  );
 }
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "../styles/row.scss";
+import Pixel from "./Pixel";
 
-export default function Row() {
-  return <div className="row">Row</div>;
+export default function Row(props) {
+  const { width, selectedColor } = props;
+
+  let pixels = [];
+  for (let i = 0; i < width; i++) {
+    pixels.push(<Pixel key={i} selectedColor={selectedColor} />);
+  }
+  return <div className="row">{pixels}</div>;
 }
